e13731449e18b2b97db6721c6809bd35a66f059e
Adriana Plečaš <131824482+D1N1A@users.noreply.github.com>
2023-06-24 23:49:20 +0200

    ispravak zadatka

--- edunova/src/Edunova04/Z01.java
+++ edunova/src/Edunova04/Z01.java
@@ -1,10 +1,29 @@
 package Edunova04;
 
+import javax.swing.JOptionPane;
+
 public class Z01 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static final int Drugi = 0;
 
+	public static void main(String[] args) {
+		int pb = Integer.parseInt(JOptionPane.showInputDialog("Prvibroj"));
+		int db = Integer.parseInt(JOptionPane.showInputDialog("Drugi broj"));
+		
+		
+		int manji = pb < db ? pb : db ;
+		int veci = pb < db ? pb : db;
+		
+		int zbroj=0;
+		
+		
+		for(int i=manji;i<=veci;i++) {
+			zbroj = i % 2 != 0 ? zbroj + i : zbroj;
+			if (i%2!=0) {
+				zbroj+=i;
+			}
+		}
+		System.out.println(zbroj);
 	}
 
 }
--- edunova/src/Edunova04/Z02.java
+++ edunova/src/Edunova04/Z02.java
@@ -3,7 +3,11 @@ package Edunova04;
 public class Z02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for (int i=100;i>=1;i--) {
+			System.out.println (i + " ,  "); 
+			
+			}
+		
 
 	}
 
