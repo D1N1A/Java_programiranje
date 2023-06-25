5ab5b3957800a0865aa01f34c631fc88a56532ce
Adriana Plečaš <131824482+D1N1A@users.noreply.github.com>
2023-06-26 01:26:35 +0200

    zadatak

--- edunova/src/Edunova03/Z06.java
+++ edunova/src/Edunova03/Z06.java
@@ -1,9 +1,21 @@
 package Edunova03;
 
+import javax.swing.JOptionPane;
+
 public class Z06 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int broj = Integer.parseInt(JOptionPane.showInputDialog("Unos broja između 1 i 999"));
+		
+		if (broj < 1 || broj > 999) {
+			System.out.println(broj + "pogrešan unos");
+		} else {
+			String brojevi = Integer.toString(broj);
+			char br [] = brojevi.toCharArray();
+			System.out.println(br[0]);
+			
+		 
+		}
 
 	}
 
