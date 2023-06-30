672a31a7ac496fc7e9a94513cc98f7da58b199ef
Adriana Plečaš <131824482+D1N1A@users.noreply.github.com>
2023-06-30 02:59:55 +0200

    zadatak

--- edunova/src/Edunova05/CiklicnaTablica.java
+++ edunova/src/Edunova05/CiklicnaTablica.java
@@ -3,27 +3,50 @@ package Edunova05;
 import javax.swing.JOptionPane;
 
 public class CiklicnaTablica {
+	
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
+	//Unos dodatnih podataka radi lakšeg pisanja koda:
+	
+	int z = 5;
+	int n = 1;
+	
+	//Unos korisnika s if uvjetnim grananjem:
+
 		
-		//Unos korisnika:
+	int red = 5 ;
+	if (red<=z)
+		red = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj redova"));
+	else
+		red = z;
+	int stupac = 5;
+	if (stupac<=z)
+		stupac = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj stupaca:"));
+	else
+		stupac = z;
 		
-		int redovi = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj redova:"));
-		int stupci = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj stupaca:"));
 		
-		// Dvodimenzionalni niz:
+	//Dvodimenzionalni niz:
+	
+		int [] [] matrica = new int [red] [stupac];
+		matrica [4] [4] =n;
+		matrica [4] [3]= n++;
 		
-		int tablica [] [] = new int [redovi] [stupci];
-				
-				
-	// Ugnježđivanje petlje (for):
 		
-		for (int i=0; i<redovi ; i++) {
-			for (int j=0 ; j<stupci ; j++) {
-				System.out.println(tablica[i][j] + "  ");
+	//Ugnježđivanje petlji:
+		
+		for (int i=0 ; i<red ; i++) {
+			for (int j=0 ; j<stupac ; j++) {
+				System.out.print(matrica[i] [j] + " ");
 			}
 			System.out.println();
 		}
-			}
-			
-	}
+	
+	//Dodavanje vrijednosti nizu:
+	
+
+		
+		
+}
+}
+		
