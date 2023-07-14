277c12db8c0bc45105f771e65425c4a76ecf5477
Adriana Plečaš <131824482+D1N1A@users.noreply.github.com>
2023-07-15 00:01:27 +0200

    ljubavni kalkulator

--- edunova/src/Edunova05/Z04ciklicnaMatrica.java
+++ edunova/src/Edunova05/Z04ciklicnaMatrica.java
@@ -1,52 +1,10 @@
 package Edunova05;
-
+import java.util.*;
 import javax.swing.JOptionPane;
 
 public class Z04ciklicnaMatrica {
 	
-public static void matrica (int [red][stupac]arr);{
 
-
-x=0;
-z=red-1;
-y=0;
-w=stupac-1;
-b=1;
-
-while (x<=z && y<=w) {
-if (b==1) {
-	for (int i=w; i>=y; --i) {
-		System.out.println(matrica[z][i] + " ");
-	}
-	--z;
-	b=4;
-	
-	else if (b==2) {
-	for (int i=z; i>=x; --i) {
-		System.out.println(matrica[i][y] + "  ");
-	}
-	++y;
-	b=1;
-	}
-	
-	else if (b ==1) {
-		for (int i = y ; i<=w; ++i) {
-			System.out.println([x][y] + "  ");
-		}
-		++z;
-		b=2;
-	}
-	else if (b==2) {
-		for (int i=x ; i<=z;++i) {
-			System.out.println(matrica[i][w]+ "  ");
-			
-		}
-		--w;
-		dir = 3;
-	}
-	
-}
-}
 
 
 public static void main(String[] args) {
@@ -57,6 +15,50 @@ public static void main(String[] args) {
 	int red= dev ? 5 : Integer.parseInt(JOptionPane.showInputDialog("Unesi broj redova"));
 	int stupac= dev ? 5 : Integer.parseInt(JOptionPane.showInputDialog("Unesi broj stupaca"));
 	
+	int  matrica [][] = new int [red] [stupac];
+	
+	int x = 0;
+	int z = red-1;
+	int y = 0;
+	int w = stupac-1;
+	int b = 1;
+
+	while (x<=z && y<=w) {
+	if (b==1) {
+		for (int i=w; i>=y; --i) {
+			System.out.println(matrica[z][i] + " ");
+		}
+		--z;
+		b=4;
+	}
+		
+		else if (b==2) {
+		for (int i=z; i>=x; --i) {
+			System.out.println(matrica[i][y] + "  ");
+		}
+		++y;
+		b=1;
+		}
+		
+		else if (b ==1) {
+			for (int i = y ; i<=w; ++i) {
+				System.out.println(matrica[x][y] + "  ");
+			}
+			++z;
+			b=2;
+		}
+		else if (b==2) {
+			for (int i=x ; i<=z;++i) {
+				System.out.println(matrica[i][w]+ "  ");
+				
+			}
+			--w;
+			b = 3;
+		}
+		
+	}
+
+	
 	for (int i = 0; i < red; i++) {
 		for (int j = 0; j < stupac; j++) {
 			System.out.print(matrica[i][j] + "\t");
@@ -66,5 +68,6 @@ public static void main(String[] args) {
 
 	
 }	
+}
 }
 
