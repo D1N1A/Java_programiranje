4329e0eef9c212e703aacd0f212c73f8cc63f3dd
Adriana Plečaš <131824482+D1N1A@users.noreply.github.com>
2023-08-08 22:59:18 +0200

    tretman

--- Zavrsni_rad/src/kozmeticki_salon/ObradaTretman.java
+++ Zavrsni_rad/src/kozmeticki_salon/ObradaTretman.java
@@ -1,10 +1,40 @@
 package kozmeticki_salon;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import kozmeticki_salon_m.Tretman;
+
+
 public class ObradaTretman {
 
-	public void prikaziIzbornik() {
-		// TODO Auto-generated method stub
-		
+	
+	private List <Tretman> tretmani;
+	private Izbornik izbornik;
+	
+	public ObradaTretman (Izbornik izbornik) {
+		this ();
+		this.izbornik = izbornik;
+	}
+	public ObradaTretman() {
+		tretmani = new ArrayList <>() {
+		}
+	}
+	
+	public void prikaziIzbornik () {
+		System.out.println("\nTretman izbornik");
+		System.out.println("1. Pregled postojećih tretmana");
+		System.out.println("2. Unos novog tretmana");
+		System.out.println("3. Promjena postojećeg tretmana");
+		System.out.println("4. Brisanje postojećeg tretmana");
+		System.out.println("5. Povratak na prethodni izbornik");
+		ucitajStavkuIzbornika();
+			
+		}
+	}
+
+	public ObradaTretman() {
+		// TODO Auto-generated constructor stub
 	}
 
 }
