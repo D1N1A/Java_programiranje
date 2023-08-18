76dac97ab0b8d7c6cdfa51006e6d1e888b2afb9d
Adriana Plečaš <131824482+D1N1A@users.noreply.github.com>
2023-08-19 00:11:57 +0200

    pokusaj popravka kalkulatora

--- edunova/src/Edunova06/kalkulator.java
+++ edunova/src/Edunova06/kalkulator.java
@@ -1,41 +1,49 @@
 package Edunova06;
 
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class kalkulator {
+	public static void main (String []args) {
+		Scanner sc=new Scanner(System.in);
+		double sum1=0,sum2=0,postotak;
+		String x;
 
-	public static void main(String[] args) {
 		
-		double zbroj1=0;
-		double zbroj2=0;
-		double postotak;
+		System.out.println("Unesi svoje ime: ");
 		
-		char ime = Integer.parseInt(JOptionPane.showInputDialog("Unesi svoje ime:"));
-		char imeS = Integer.parseInt(JOptionPane.showInputDialog("Unesi ime simpatije:"));
+		String s1=sc.nextLine();
+		System.out.println( "Unesi ime simpatije: ");
+		String s2=sc.nextLine();
+		char[] arr=s1.toCharArray();
+		char[] arr1=s2.toCharArray();
+		int b=0; int i = 0;
 		
-		int ime1  = new int [ime1];
-		int[] imeS = new int [imeS];
+		for(char ch : arr1)
+		{
+			
+			b=(int) arr1[i++];
+			int sum11 = sum11+b;
+			
+		}
 		
-		int b=0;
-		int i=0;
-		
-		for (char ch
-				: ime)
+		char[] arr2;
+		for (char ch1 : arr2)
 		{
 			int i1;
-			b=(int) ime1[i1++]
-					sum1=sum1+b;
+			int[] arr21 = null;
+			b=(int)arr21[i1++];
+			int 
+;
+			int sum = sum21+b;
 		}
-		i=0;
-		for (int i : imeS)
-		{
-			b=(int) imeS [i++];
-			sum2=sum+b;
-		}
-		if (zbroj1<=zbroj2)*100;
+		if (sum1<=sum2)
+			postotak=(sum1/sum2)*100
+;
 		else
-			posto=(zbroj2/zbroj1)*100;
-		System.out.println("Postotak zaljubljenosti je ="+posto);
-	}
-
+			postotak=(sum2/sum1)*100;
+		System.out.println(""
+				+ " Ljubavni postotak: ");
+		
 }
+}
+
