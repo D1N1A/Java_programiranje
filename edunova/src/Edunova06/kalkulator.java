5fc88c81ac82e903caf0f41258320971222067ec
Adriana Plečaš <131824482+D1N1A@users.noreply.github.com>
2023-08-20 01:16:28 +0200

    Kalkulator

--- edunova/src/Edunova06/kalkulator.java
+++ edunova/src/Edunova06/kalkulator.java
@@ -2,48 +2,38 @@ package Edunova06;
 
 import java.util.Scanner;
 
-public class kalkulator {
-	public static void main (String []args) {
-		Scanner sc=new Scanner(System.in);
-		double sum1=0,sum2=0,postotak;
-		String x;
 
-		
-		System.out.println("Unesi svoje ime: ");
-		
+public class kalkulator {
+
+	public static void main(String[] args) {
+		double sum1=0,sum2=0,postotak;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Vaše ime :");
 		String s1=sc.nextLine();
-		System.out.println( "Unesi ime simpatije: ");
-		String s2=sc.nextLine();
-		char[] arr=s1.toCharArray();
-		char[] arr1=s2.toCharArray();
-		int b=0; int i = 0;
-		
-		for(char ch : arr1)
-		{
-			
+		System.out.println("Ime vaše simpatije: ");
+		String s2=sc.next();
+		char[] arr1=s1.toCharArray();
+		char[] arr2=s2.toCharArray();
+		int b=0, i=0;
+		for (char ch : arr1)
+		 {
 			b=(int) arr1[i++];
-			int sum11 = sum11+b;
-			
-		}
+			sum1=sum1+b;
+		 }
+	
+		i=0;
+		for(char ch : arr2)
+		 {
+			b=(int) arr2[i++];
+			sum2=sum2+b;
+		 }
+	if (sum1<=sum2)
+		postotak=(sum1/sum2)*100;
+	else
+		postotak=(sum2/sum1)*100;
+	System.out.println("Postotak zaljubljenosti je = "+postotak);
 		
-		char[] arr2;
-		for (char ch1 : arr2)
-		{
-			int i1;
-			int[] arr21 = null;
-			b=(int)arr21[i1++];
-			int 
-;
-			int sum = sum21+b;
-		}
-		if (sum1<=sum2)
-			postotak=(sum1/sum2)*100
-;
-		else
-			postotak=(sum2/sum1)*100;
-		System.out.println(""
-				+ " Ljubavni postotak: ");
-		
-}
+	}
+	
 }
 
