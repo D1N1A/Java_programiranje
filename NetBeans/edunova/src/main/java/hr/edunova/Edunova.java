aec72abbfe72d5c437de392ba076faf414d2984f
Adriana Plečaš <131824482+D1N1A@users.noreply.github.com>
2023-08-25 18:38:10 +0200

    .

---
+++ NetBeans/edunova/src/main/java/hr/edunova/Edunova.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hr.edunova;
+
+/**
+ *
+ * @author Ana
+ */
+public class Edunova {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
